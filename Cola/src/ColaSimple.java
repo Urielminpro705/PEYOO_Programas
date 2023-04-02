ad8fe677f7987226d35c913fd9e4bdcad7216c15
Urielminpro705 <urielmata37@hotmail.com>
2023-04-01 21:09:36 -0600

    :(

--- Cola/src/ColaSimple.java
+++ Cola/src/ColaSimple.java
@@ -1,13 +1,13 @@
 public class ColaSimple{
     char[] cola;
-    int inicio, fin;
+    int inicio, fin=-1;
     
     public ColaSimple(int tam){
         cola=new char[tam];
     }
 
 
-    public void insertar(char elemento){
+    /*public void insertar(char elemento){
         if(fin==-1){
             inicio=0;
             fin=0;
@@ -20,9 +20,50 @@ public class ColaSimple{
         else{
             System.out.println("No hay espera");
         }
+    }*/
+
+    public void insertar(char elemento){
+        if(fin==-1){
+            fin=0;
+            inicio=0;
+            cola[fin]=elemento;
+        }
+        else{
+            if(fin<cola.length){
+                fin++;
+                cola[fin]=elemento;
+            }       
+        }      
     }
 
-    public void eliminar(char elemento){
+    public void eliminar(){
+        
+    }
+
+    public void imprimir(){   
+        for(int i=0; i<cola.length;i++){
+            System.out.print(cola[i]);
+        } 
+        System.out.println("\n");      
+    }
+
+    /*public void eliminar(){        
+        if(inicio<=fin){  
+            if(fin==-1){
+                System.out.println("No hay elementos en la cola");
+            }         
+            if(fin>=0 && fin<cola.length){
+                cola[inicio]='\0';
+                inicio++;
+            }
+            else{
+                inicio=-1;
+                fin=-1;
+            } 
+        }          
+    }*/
+
+    /*public void eliminar(char elemento){
         if(inicio<fin){
             if(fin>=0){
                 cola[inicio]='\0';
@@ -34,5 +75,5 @@ public class ColaSimple{
                 System.out.println("Se reinicia la cola");
             }
         }
-    }
+    }*/
 }
--- Cola/src/PEYOO_MCU.java
+++ Cola/src/PEYOO_MCU.java
@@ -1,14 +1,38 @@
+import java.util.Scanner;
 public class PEYOO_MCU{
     public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
         ColaSimple a=new ColaSimple(5);
-        a.insertar('a');
-        a.insertar('b');
-        a.insertar('c');
-        a.insertar('d');
-        a.eliminar('a');
-        a.eliminar('b');
-        a.eliminar('c');
-        a.eliminar('d');
-        a.eliminar('g');
+        char x;
+        int opcion;
+        do{
+            opcion=menu();
+            switch(opcion){
+                case 1:
+                    System.out.println("Introduce un valor");
+                    a.insertar(x=input.next().charAt(0));
+                    break;
+    
+                case 2:
+                    a.eliminar();
+                break;
+
+                case 3:
+                    a.imprimir();
+                    break;
+            }
+        }while(opcion!=7);
+        
+    }
+
+    public static int menu(){
+        Scanner input=new Scanner(System.in);
+        int opcion;
+        System.out.println("Escoge una opcion");
+        System.out.println("1) Agregar a la cola");
+        System.out.println("2) Sacar de la cola");
+        System.out.println("3) Ver la cola");
+        opcion=input.nextInt();
+        return opcion;
     }
 }
