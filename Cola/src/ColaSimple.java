3d028715798038dd3958f5d767ce458d53e884cc
Urielminpro705 <urielmata37@hotmail.com>
2023-04-01 22:54:02 -0600

    Terminado

--- Cola/src/ColaSimple.java
+++ Cola/src/ColaSimple.java
@@ -9,36 +9,6 @@ public class ColaSimple{
         aux=new char[tam];
     }
 
-
-    /*public void insertar(char elemento){
-        if(fin==-1){
-            inicio=0;
-            fin=0;
-            cola[fin]=elemento;
-        }
-        if(fin<cola.length){
-            fin++;
-            cola[fin]=elemento;
-        }
-        else{
-            System.out.println("No hay espera");
-        }
-    }*/
-
-    /*public void insertar(char elemento){
-        if(fin==-1){
-            fin=0;
-            inicio=0;
-            cola[fin]=elemento;
-        }
-        else{
-            if(fin<cola.length){
-                fin++;
-                cola[fin]=elemento;
-            }       
-        }      
-    }*/
-
     public void insertar(char elemento){
         if(fin==-1){
             fin=0;
@@ -58,52 +28,28 @@ public class ColaSimple{
         if(fin==-1){
             System.out.println("No hay elementos en la cola");
         }
-        else{     
-            i=0;      
-            do{
-                aux[i]=cola[i+1];
-                i++;
-            }while(i<contador);
+        else{
+            if(contador==cola.length){
+                for(int i=0; i<contador-1;i++){
+                    aux[i]=cola[i+1];
+                }
+            }
+            else{
+                for(int i=0; i<contador;i++){
+                    aux[i]=cola[i+1];
+                }
+            }            
             cola=aux;
             contador--;
             fin=contador-1;
-        }       
+        }
+         
     }
 
     public void imprimir(){   
-        for(int i=0; i<cola.length;i++){
+        for(int i=0; i<contador;i++){
             System.out.print(cola[i]);
         } 
         System.out.println("\n");      
     }
-
-    /*public void eliminar(){        
-        if(inicio<=fin){  
-            if(fin==-1){
-                System.out.println("No hay elementos en la cola");
-            }         
-            if(fin>=0 && fin<cola.length){
-                cola[inicio]='\0';
-                inicio++;
-            }
-            else{
-                inicio=-1;
-                fin=-1;
-            } 
-        }          
-    }*/
-
-    /*public void eliminar(char elemento){
-        if(inicio<fin){
-            if(fin>=0){
-                cola[inicio]='\0';
-                inicio++;
-            }
-            else{
-                inicio=-1;
-                fin=-1;
-                System.out.println("Se reinicia la cola");
-            }
-        }
-    }*/
 }
