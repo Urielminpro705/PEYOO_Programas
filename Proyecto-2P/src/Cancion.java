a8a85d92ef8b199f1d5752a8cb006947cf3defe1
Urielminpro705 <urielmata37@hotmail.com>
2023-04-11 19:03:04 -0600

    v1.3

---
+++ Proyecto-2P/src/Cancion.java
@@ -0,0 +1,11 @@
+public class Cancion {
+    public String nombre;
+    public double duracion;
+
+    public Cancion(){}
+
+    public Cancion(String nombre, double duracion){
+        this.nombre=nombre;
+        this.duracion=duracion;
+    }
+}
