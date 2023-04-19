6080c6220c2cbd8ccf8abad2f8ab4aa2582492d5
Uriel Mata Castellanos <urielmata37@hotmail.com>
2023-04-19 00:55:10 -0600

    XD

--- ColaCircular/src/ColaCircular/ColaCircular.java
+++ ColaCircular/src/ColaCircular/ColaCircular.java
@@ -37,61 +37,53 @@ public class ColaCircular{
     }*/
 
     public void insertar(char elemento){
-        if(fin==cola.length){
-            fin=0;
-        }else{
-            if(fin==inicio){
-                System.out.println("No hay espacio");
-            }
-            else{
-                if(fin==-1){
-                    fin=0;
-                    cola[fin]=elemento;
-                    contador++;
+        if(contador==cola.length){
+            System.out.println("No hay espacio");
+        }
+        else{
+            if(fin==cola.length){
+                fin=0;
+            }else{
+                if(fin==inicio && inicio!=-1){
+                    System.out.println("No hay espacio");
                 }
                 else{
-                    if(fin<cola.length){
-                        if(inicio==-1){
-                            fin++;
-                            cola[fin]=elemento;
-                            contador++;
-                        }
-                        else{
-                            if(inicio>-1){
-                                fin++; 
+                    if(fin==-1){
+                        fin=0;
+                        cola[fin]=elemento;
+                        contador++;
+                    }
+                    else{
+                        if(fin<cola.length){
+                                fin++;
                                 cola[fin]=elemento;
-                                contador++;   
-                            } 
-                        }
-                        
-                    }       
-                }    
-            }
-        }        
+                                contador++;
+                        }       
+                    }    
+                }
+            }        
+        }       
     }
 
     public void eliminar(){
-        if(contador==0){
+        if(contador == 0){
             System.out.println("No hay elementos en la cola");
         }
         else{
-            if(inicio==cola.length){
-                inicio=-1;
+            if(inicio == cola.length){
+                inicio = -1;
             }
-            else{
-                if(inicio==-1){
-                    inicio=0;
+            if(inicio == -1){
+                inicio=0;
+                cola[inicio]='\0';
+                contador--;
+            } else{
+                if(inicio > -1){
+                    inicio++;
                     cola[inicio]='\0';
                     contador--;
                 }
-                else{
-                    if(inicio>-1){
-                        inicio++;
-                        cola[inicio]='\0';
-                        contador--;
-                    }
-                }
-            }          
+            }         
         }        
     }
 
@@ -113,21 +105,28 @@ public class ColaCircular{
         }        
     }*/
 
-    public void imprimir(){
-        if(inicio==-1){
-            for(int i=0; i<contador;i++){
-                System.out.print(cola[i]);
-            } 
-            System.out.println("\n");   
-        }
-        else{
-            if(inicio>-1){
-                for(int j=inicio; j<(fin-inicio);j++){
-                    System.out.print(cola[j]);
-                }                   
+    // public void imprimir(){
+    //     if(inicio==-1){
+    //         for(int i=0; i<contador;i++){
+    //             System.out.print(cola[i]);
+    //         } 
+    //         System.out.println("\n");   
+    //     }
+    //     else{
+    //         if(inicio>-1){
+    //             for(int j=inicio; j<(fin-inicio);j++){
+    //                 System.out.print(cola[j]);
+    //             }                   
                 
-                System.out.println("\n");   
-            }       
-        }              
+    //             System.out.println("\n");   
+    //         }       
+    //     }              
+    // }
+
+    public void imprimir(){
+        for(int i=0; i<contador;i++){
+            System.out.print(cola[i]);
+        } 
+        System.out.println("\n");         
     }
 }
