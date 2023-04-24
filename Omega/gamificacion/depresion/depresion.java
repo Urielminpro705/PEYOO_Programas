0d7c2ac5b5403f71f65a9d02d1da27f1920f40e3
Urielminpro705 <urielmata37@hotmail.com>
2023-04-23 22:43:14 -0600

    XD

--- Omega/gamificacion/depresion/depresion.java
+++ Omega/gamificacion/depresion/depresion.java
@@ -4,12 +4,28 @@ import java.util.Scanner;
 public class depresion {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
-        int amigos, n, estomago;
+        int amigos, n;
+        int[] estomago;
+        int[] c;
+        int[] v;
         n=input.nextInt();;
+        c = new int[n];
+        v = new int[n];
         for(int i=0; i < n; i++){
-            
+            c[i] = input.nextInt();
+            v[i] = input.nextInt();
+        }
+        
+        amigos = input.nextInt();
+        estomago = new int[amigos];
+        for(int i = 0; i < amigos; i++){
+            estomago[i] = input.nextInt();
         }       
-    }   
+    }
+
+    public static void comprobar(){
+        
+    }
 }
 
 
