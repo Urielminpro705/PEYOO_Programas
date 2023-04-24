d9b198b0b07c5ff1fbb40fa4fbd8f09befb60e88
Urielminpro705 <urielmata37@hotmail.com>
2023-04-23 21:44:08 -0600

    XD

---
+++ Omega/gamificacion/depresion/depresion.java
@@ -0,0 +1,15 @@
+package Omega.gamificacion.depresion;
+import java.util.Scanner;
+
+public class depresion {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int amigos, n, estomago;
+        n=input.nextInt();;
+        for(int i=0; i < n; i++){
+            
+        }       
+    }   
+}
+
+
