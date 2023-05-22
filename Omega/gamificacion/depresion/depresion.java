3d5e9163e3173384cbbc18b27fc621bc3ef352af
Uriel Mata Castellanos <urielmata37@hotmail.com>
2023-05-22 15:36:16 -0600

    XD

--- Omega/gamificacion/depresion/depresion.java
+++ Omega/gamificacion/depresion/depresion.java
@@ -3,29 +3,34 @@ import java.util.Scanner;
 
 public class depresion {
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
-        int amigos, n;
-        int[] estomago;
-        int[] c;
-        int[] v;
-        n=input.nextInt();;
-        c = new int[n];
-        v = new int[n];
-        for(int i=0; i < n; i++){
-            c[i] = input.nextInt();
-            v[i] = input.nextInt();
+        Scanner input = new Scanner(System.in);
+        int n, pos, a, e;
+        botella mayor = new botella();
+        n = input.nextInt();
+        botella[] bot = new botella(n);
+        for(int i = 0; i < bot.length; i++){
+            bot[i].calorias = input.nextInt();
+            bot[i].volumen = input.nextInt();
         }
         
-        amigos = input.nextInt();
-        estomago = new int[amigos];
-        for(int i = 0; i < amigos; i++){
-            estomago[i] = input.nextInt();
-        }       
-    }
-
-    public static void comprobar(){
-        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(bot[j].calorias > mayor.calorias && bot[j].disponible == true){
+                    mayor = bot[j];
+                    pos = j;
+                }
+            }
+            if(mayor.volumen <= e){
+                bot[pos].disponible = false;
+            }
+        }     
     }
 }
 
+public class botella{
+    public int volumen;
+    public int calorias;
+    public Boolean disponible = true;
 
+    public botella(){}
+}
