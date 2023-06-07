08867ce65fb24720325938d4d03079c745481f16
Uriel Mata Castellanos <urielmata37@hotmail.com>
2023-06-07 00:20:22 -0600

    XD

--- Arbolito/arbolito.java
+++ Arbolito/arbolito.java
@@ -1,5 +1,82 @@
 package Arbolito;
 
 public class arbolito {
-    
+    //Se declara el nodo que va a ser la raiz
+    nodito raiz;
+    //Metodo constructor
+    public arbolito () {
+        raiz = null;
+    }
+
+    //Metodo para insertar un nuevo nodo
+    public void insertar (int e, String n){
+        //Se declara un nodo nuevo
+        nodito nuevo;
+        //Se inicializa el nodo con la edad y el nombre
+        nuevo = new nodito(e,n);
+        //If que comprueba que es la primera raiz
+        if (raiz == null)
+            //Se guarda la raiz como nueva
+            raiz = nuevo;
+        else{
+            //Se crea un nodo y se hace null
+            nodito ant = null;
+            //Se crea un nodo auxiliar
+            nodito aux;
+            //Se guarda la raiz en el auxiliar
+            aux = raiz;
+            //Ciclo para encontrar donde se debe insertar el nuevo nodo
+            while (aux != null){
+                ant = aux;
+                if (e <= aux.edad){
+                    aux = aux.izq;
+                }
+                else{
+                    aux = aux.der;
+                }
+            }
+            if (e <= ant.edad){
+                ant.izq = nuevo;
+            }
+            else{
+                ant.der = nuevo;
+            }
+        }
+    }
+
+    //Metodo para mostrar el recorrido en preorden
+    public void preorden(nodito r){
+        if(r != null){
+            //Imprimir nombre y edad
+            System.out.println(r.nombre + " tiene " + r.edad);
+            //Se llama al mismo metodo por la izquierda
+            preorden(r.izq);
+            //Se llama al mismo metodo por la derecha
+            preorden(r.der);
+        }
+    }
+
+    //Metodo para mostrar el recorrido en inorden
+    public void inorden(nodito r){
+        if(r != null){
+            //Se llama al mismo metodo por la izquierda
+            preorden(r.izq);
+            //Imprimir nombre y edad
+            System.out.println(r.nombre + " tiene " + r.edad);
+            //Se llama al mismo metodo por la derecha
+            preorden(r.der);
+        }
+    }
+
+    //Metodo para mostrar el recorrido en postorden
+    public void postorden(nodito r){
+        if(r != null){
+            //Se llama al mismo metodo por la izquierda
+            preorden(r.izq);
+            //Se llama al mismo metodo por la derecha
+            preorden(r.der);
+            //Imprimir nombre y edad
+            System.out.println(r.nombre + " tiene " + r.edad);
+        }
+    }
 }
--- Arbolito/nodito.java
+++ Arbolito/nodito.java
@@ -1,5 +1,22 @@
 package Arbolito;
 
 public class nodito {
-    
+    //Atributo de nombre
+    String nombre;
+    //Atributo de edad
+    int edad;
+    //Atributo para direccionar los siguientes nodos
+    nodito izq, der;
+
+    //Metodo constructor
+    public nodito(int edad, String nombre){
+        //Se iguala el atributo de edad con el que se inserta
+        this.edad = edad;
+        //Se iguala el atributo de nombre con el que se inserta
+        this.nombre = nombre;
+        //Se hace null al atributo de izquierda
+        izq = null;
+        //Se hace null al atributo de derecha
+        der = null;
+    }
 }
